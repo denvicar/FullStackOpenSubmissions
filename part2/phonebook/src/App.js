b913eb8dda9bfc42a966d222749927006b5202b4
Ciro Faenza <rkumura3@gmail.com>
2021-10-26 01:58:18 +0200

    exercise2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '039-44-559'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const handleChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const handleSubmit = (event) => {
@@ -15,10 +20,12 @@ const App = () => {
     if(persons.map(p=>p.name).includes(newName)) {
       alert(`${newName} already exists!`)
       setNewName('')
+      setNewNumber('')
     } else {
-      const newPerson = {name: newName}
+      const newPerson = {name: newName, number: newNumber}
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -28,13 +35,14 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleChange} value={newName} />
+          number: <input onChange={handleChangeNumber} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p=><p key={p.name}>{p.name}</p>)}
+      {persons.map(p=><p key={p.name}>{p.name} {p.number}</p>)}
     </div>
   )
 }
