5bbd4b40d02262f33309b80e1de8a6d0e5c5623e
Ciro Faenza <rkumura3@gmail.com>
2021-10-26 01:11:55 +0200

    exercise2.2

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -8,7 +8,7 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((acc,curr)=>acc+curr.exercises,0)
   return(
     <p>Number of exercises {sum}</p>
   ) 
@@ -35,6 +35,7 @@ const Course = ({course}) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
